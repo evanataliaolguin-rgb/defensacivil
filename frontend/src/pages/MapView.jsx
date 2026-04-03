5ac4beb84ca4d79d51ffa4e0cf4db201d3864388
evanataliaolguin-rgb <evanataliaolguin4@gmail.com>
2026-04-03 14:30:10 -0300

    fix: importar JS de leaflet.markercluster para que L.markerClusterGroup funcione

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -7,6 +7,7 @@ import {
 } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import { incidentsApi } from '../api/incidents.api';
